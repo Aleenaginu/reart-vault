cb84ddf2ec0d031be2bb98eb747aa37495ed755d
Aleenaginu <78251467+Aleenaginu@users.noreply.github.com>
2025-04-04 14:43:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 # exit on error
 set -o errexit
 
-# Install system dependencies required for dlib and opencv
-apt-get update
-apt-get install -y cmake libgl1-mesa-glx libglib2.0-0 libsm6 libxext6 libxrender-dev python3-dev
-
 # Install Python dependencies
 pip install --upgrade pip
-pip install -r requirements.txt 
+pip install wheel
+pip install cmake
+pip install --no-cache-dir dlib==19.24.0
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input 
