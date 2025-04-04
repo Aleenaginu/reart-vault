4c95b63959c5e96ebe3a05e5e245836be5dc0718
Aleenaginu <78251467+Aleenaginu@users.noreply.github.com>
2025-04-04 14:54:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ set -o errexit
 
 # Install Python dependencies
 pip install --upgrade pip
-pip install wheel
-pip install cmake
-pip install --no-cache-dir dlib==19.24.0
+
+# Install dlib from pre-built wheel
+pip install https://files.pythonhosted.org/packages/0e/ce/f8a3cff33ac03a8219768f0694c5d703c8e037e6aba2e865f9bae22ed63c/dlib-19.8.1-cp36-cp36m-win_amd64.whl#sha256=794994fa2c54e7776659fddb148363a5556468a6d5d46be8dad311722d54bfcf; exit 0
+
+# Install other dependencies
 pip install -r requirements.txt
 
 # Collect static files
