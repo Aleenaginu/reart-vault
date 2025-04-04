bb1d43b86b864312263db151685f918b297a5c1a
Aleenaginu <78251467+Aleenaginu@users.noreply.github.com>
2025-04-04 15:05:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,12 +5,7 @@ set -o errexit
 # Install Python dependencies
 pip install --upgrade pip
 
-# Install face recognition dependencies using specific versions
-pip install --no-cache-dir numpy==1.24.3
-pip install --no-cache-dir "dlib @ https://github.com/jloh02/dlib/releases/download/v19.24/dlib-19.24.0-cp311-cp311-linux_x86_64.whl"
-pip install --no-cache-dir face-recognition
-
-# Install other dependencies
+# Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
