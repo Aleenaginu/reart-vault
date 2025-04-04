0e1e7f571c885cfbc800fc462939cf5d293b51a5
Aleenaginu <78251467+Aleenaginu@users.noreply.github.com>
2025-04-04 15:14:08 +0530

    Update settings.py

--- reart/settings.py
+++ reart/settings.py
@@ -153,10 +153,14 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL= 'media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+
+# WhiteNoise Configuration
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_USE_FINDERS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
